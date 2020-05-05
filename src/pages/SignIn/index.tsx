edbde70128a1feda1b19363211ea32aac2ada388
heluansantos <heluan.caetano@gmail.com>
2020-05-05 16:15:30 -0300

    Densidade de pixel

--- src/pages/SignIn/index.tsx
+++ src/pages/SignIn/index.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
+import { Image } from 'react-native';
 import { View } from 'react-native';
 import { Container } from './styles';
+import logoImg from '../../assets/logo.png';
 
 const SigIn: React.FC = () => {
   return (
     <Container>
-      <View />
+      <Image source={logoImg} />
     </Container>
   );
 };
