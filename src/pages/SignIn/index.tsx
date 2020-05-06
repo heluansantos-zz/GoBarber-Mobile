eae001cadda4003de91bc87913ba23ac45f966d0
heluansantos <heluan.caetano@gmail.com>
2020-05-05 21:32:04 -0300

    Input e button

--- src/pages/SignIn/index.tsx
+++ src/pages/SignIn/index.tsx
@@ -3,12 +3,18 @@ import { Image } from 'react-native';
 import { View } from 'react-native';
 import { Container, Title } from './styles';
 import logoImg from '../../assets/logo.png';
+import Input from '../../components/Input';
+import Button from '../../components/Button';
 
 const SigIn: React.FC = () => {
   return (
     <Container>
       <Image source={logoImg} />
       <Title>Faça seu logon</Title>
+
+      <Input name="email" icon="mail" placeholder="E-mail" />
+      <Input name="password" icon="lock" placeholder="Senha" />
+      <Button onPress={() => {}}>Entrar</Button>
     </Container>
   );
 };
